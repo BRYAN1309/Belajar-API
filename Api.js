c1f126f459e5a32ea59d790848de3f0117a4ef53
BRYAN1309 <bryanthanaya1@gmail.com>
2025-02-21 23:35:17 +0700

    simple get

--- Api.js
+++ Api.js
@@ -1,2 +1,9 @@
-let x = 34;
-console.log(x);
+const url = "https://api.api-ninjas.com/v1/worldtime?city=london";
+
+async function getData() {
+    const resp = await fetch(url);
+    console.log(resp)
+    
+} 
+
+getData();
